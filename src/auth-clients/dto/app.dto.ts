a6ef16eb5db5018a617cf0340288ea56e7a51a19
marioVDev <mariogdev26@gmail.com>
2025-09-29 18:54:41 -0600

    Feat: AppDto Added

--- src/auth-clients/dto/app.dto.ts
+++ src/auth-clients/dto/app.dto.ts
@@ -1,24 +1,21 @@
-import { IsString } from 'class-validator';
+import { IsString, IsNotEmpty } from 'class-validator';
 
 /**
  * Data Transfer Object for creating a new app
  */
 export class CreateAppDto {
   @IsString()
-  clientId: string;
-
-  @IsString()
+  @IsNotEmpty()
   name: string;
 
   @IsString()
   description?: string;
+}
+
+export class UpdateAppDto {
+  @IsString()
+  name?: string;
 
   @IsString()
-  plan?: string;
-
-  @IsString()
-  jwtSecret: string;
-
-  @IsString()
-  jwtExpiresIn: string;
+  description?: string;
 }
