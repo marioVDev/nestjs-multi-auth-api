81edb1eb282b4474adef165e04d816d65ad7dcf6
marioVDev <mariogdev26@gmail.com>
2025-09-29 19:09:45 -0600

    Update: AppRepository with all querys needed

--- src/auth-clients/repositories/app.repository.ts
+++ src/auth-clients/repositories/app.repository.ts
@@ -1,8 +1,9 @@
 import { PrismaService } from 'src/db/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { BaseRepository } from './base.repository';
-import { RegisterAppDto } from '../dto/app.dto';
+import { CreateAppDto, UpdateAppDto } from '../dto/app.dto';
 import { PrismaClient } from 'generated/prisma';
+import { App } from 'generated/prisma';
 
 @Injectable()
 export class AppRepository extends BaseRepository {
@@ -10,20 +11,89 @@ export class AppRepository extends BaseRepository {
     super(prisma);
   }
 
-  async createApp(data: RegisterAppDto, prismaClient?: PrismaClient) {
-    const { clientId, name, jwtSecret, jwtExpiresIn } = data;
-
-    if (!clientId || !name || !jwtSecret || !jwtExpiresIn) {
-      throw new Error('Missing required fields');
-    }
+  /**
+   * Create a new app for a client can use the auth system.
+   * @param data - Data to create the app (name, description).
+   * @param clientId - ID of the client (Received from the AppService).
+   * @param jwtSecret - JWT secret for the app (Generated automatically).
+   * @param prismaClient - Optional Prisma client instance (For transactions management).
+   * @returns A promise that resolves to the created app.
+   */
+  async createApp(
+    data: CreateAppDto,
+    clientId: string,
+    jwtSecret: string,
+    prismaClient?: PrismaClient,
+  ): Promise<App> {
+    const { name, description } = data;
 
     return this.transaction(prismaClient).app.create({
       data: {
         clientId,
         name,
         jwtSecret,
-        jwtExpiresIn,
+        description,
       },
+    });
+  }
+
+  /**
+   * Update an existing app's details.
+   * @param appId - ID of the app to be updated.
+   * @param data - Data to update the app (name, description).
+   * @param clientId - ID of the client (To ensure the app belongs to the client).
+   * @param prismaClient - Optional Prisma client instance (For transactions management).
+   * @returns A promise that resolves the updated app.
+   */
+  async updateApp(
+    appId: string,
+    clientId: string,
+    data: UpdateAppDto,
+    prismaClient?: PrismaClient,
+  ): Promise<App> {
+    return this.transaction(prismaClient).app.update({
+      where: { id: appId, clientId },
+      data,
+    });
+  }
+
+  /**
+   * Delete an existing app.
+   * @param appId - ID of the app to be deleted.
+   * @param clientId - ID of the client (To ensure the app belongs to the client).
+   * @param prismaClient - Optional Prisma client instance (For transactions management).
+   * @returns A promise that resolves the deleted app.
+   */
+  async deleteApp(
+    appId: string,
+    clientId: string,
+    prismaClient?: PrismaClient,
+  ): Promise<App> {
+    return this.transaction(prismaClient).app.delete({
+      where: { id: appId, clientId },
+    });
+  }
+
+  /**
+   * Get an existing app by its ID.
+   * @param appId - ID of the app to be retrieved.
+   * @param clientId - ID of the client (To ensure the app belongs to the client).
+   * @returns A promise that resolves to the found app or null if not found.
+   */
+  async getAppById(appId: string, clientId: string): Promise<App | null> {
+    return this.prisma.app.findFirst({
+      where: { id: appId, clientId },
+    });
+  }
+
+  /**
+   * Get all apps for a specific client.
+   * @param clientId - ID of the client (To ensure the apps belong to the client).
+   * @returns A promise that resolves to an array of apps.
+   */
+  async getAllAppsByClientId(clientId: string): Promise<App[]> {
+    return this.prisma.app.findMany({
+      where: { clientId },
     });
   }
 }
